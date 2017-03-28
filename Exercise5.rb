3307d681cc8d9fd183eec05f95ff02b2298bfc18
EDWIN AU <edwinau@rogers.com>
2017-03-28 14:18:47 -0400

    Ex5 - reverse name

---
+++ Exercise5.rb
@@ -0,0 +1,9 @@
+def greet_backwards(name)
+  puts "Your future name is " + name.reverse
+end
+
+greet_backwards("James")
+greet_backwards("Bob")
+greet_backwards("Shirly")
+greet_backwards("Sue")
+greet_backwards("Andy")
