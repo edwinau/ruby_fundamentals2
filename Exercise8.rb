0eeccb8a85fd374d624049b51161948cd8983080
EDWIN AU <edwinau@rogers.com>
2017-03-28 16:19:42 -0400

    Ex8 +Ex9 Complete

--- Exercise8.rb
+++ Exercise8.rb
@@ -1,9 +1,21 @@
-expense = [1,2,3,4]
-sum = 0
-
-expense.each do |x|
-  puts sum += x
+def expensesum(exp_array)
+  sum = 0
+  exp_array.each do |x|
+    puts sum += x
+  end
+  return sum
 end
 
+expense1 = [1,2,3,4]
+
+total1 = expensesum(expense1)
+
 puts "----"
-puts "Your expense total is $#{sum}."
+puts "Your expense total is $#{total1}."
+
+expense2 = [5,6,7,8]
+
+total2 = expensesum(expense2)
+
+puts "----"
+puts "Your expense total is $#{total2}."
--- Exercise9.rb
+++ Exercise9.rb
@@ -1,7 +1,22 @@
 grocery_list = ["carrots", "detergent", "meat", "fruits", "apple"]
 
-grocery_list.each do |x|
-  puts "*" + x.to_s
+# puts grocery_list.length
+
+#1
+# def print_list(list)
+  list.each do |x|
+    puts "*" + x.to_s
+  end
+  puts "-------------"
+#end
+
+def additem(list, item)
+  list << "*#{item}"
+  # print_list(list)
 end
 
-puts grocery_list << "celery"
+additem(grocery_list, "juice")
+additem(grocery_list, "bananas")
+#2 lost count out put totale
+
+ #puts grocery_list.length
