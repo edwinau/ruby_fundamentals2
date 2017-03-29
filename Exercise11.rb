6ff3d12ed11638a3db6493f9ccc9734f8fac647f
EDWIN AU <edwinau@rogers.com>
2017-03-29 07:04:19 -0400

    Ex11 - Array/Hash entry

--- Exercise11.rb
+++ Exercise11.rb
@@ -1,5 +1,11 @@
-my_dogs = [
-  { :name => 'Ralph', :position => 5 }
-  { :name => 'Cindy', :position => 8 }
-  { :name => 'Jade', :position => 11 }
-]
+my_dogs = [{ :name => 'Ralph', :position => 5 },{ :name => 'Cindy', :position => 8 },{ :name => 'Jade', :position => 11 },]
+#1
+my_dogs.each do |x,y|
+  puts x + y
+end
+
+#1.Figure out which dogs have left the backyard. Write a method called get_absent_dogs that takes an array of dogs
+#  (hashes) as an argument and returns an array of dogs (that are absent).
+
+
+#method get_absent_dogs
