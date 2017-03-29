c9d270a4db4d4b6a848af5d18ad1833cbffed069
EDWIN AU <edwinau@rogers.com>
2017-03-28 21:56:36 -0400

    Ex10 - print

--- Exercise10.rb
+++ Exercise10.rb
@@ -1,13 +1,15 @@
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
-  :cohort3 => 22,
+  :cohort3 => 22
 }
 
-classes(x,y) do |x,y|
-  puts "#{x}: #{y} students"
+students.each do |x,y|
+  list = "#{x}: #{y} students"
+  puts list
 end
 
+
 puts "----"
-puts classes(students)
+puts students
 puts "----"
