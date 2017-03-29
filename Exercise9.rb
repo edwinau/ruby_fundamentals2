18ab730687248ec8f16d0c6a9a39808e73c893b4
EDWIN AU <edwinau@rogers.com>
2017-03-28 20:18:42 -0400

    Ex9 complete

--- Exercise9.rb
+++ Exercise9.rb
@@ -1,18 +1,19 @@
-grocery_list = ["carrots", "juice", "apple", "celery"]
+original_grocery_list = ["carrots", "juice", "salmon", "apple", "celery"]
 
-grocery_list.map do |list|
-    puts "* #{list}"
+
+grocery_list = original_grocery_list.map do |list|
+    "* #{list}"
 end
-
+puts grocery_list
 puts "------"
 
 def additem(grocery_list, object)
-  grocery_list << "*"+object
+  grocery_list << "* "+object
   puts "* #{object}"
 end
 
 
-additem(grocery_list,"juice")
+additem(grocery_list,"honey")
 additem(grocery_list,"cranberry")
 
 puts "------"
@@ -31,3 +32,8 @@ end
 puts "------"
 
 puts grocery_list[1]
+puts "------"
+puts grocery_list.sort
+puts "------"
+grocery_list.delete("* salmon")
+puts grocery_list
