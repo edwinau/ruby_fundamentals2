38cfa8f93db67e522bdca7a42008cc81fac4eaa1
EDWIN AU <edwinau@rogers.com>
2017-03-28 22:03:24 -0400

    added cohort 4

--- Exercise10.rb
+++ Exercise10.rb
@@ -1,15 +1,21 @@
+#1
+
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
 
-students.each do |x,y|
+#2
+students.each.map do |x,y|
   list = "#{x}: #{y} students"
   puts list
 end
 
-
 puts "----"
 puts students
 puts "----"
+
+#3
+students["cohort4"] = "43"
+puts students
