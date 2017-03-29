00ae00999a3285b507ca993635d5518cb9155fa9
EDWIN AU <edwinau@rogers.com>
2017-03-29 08:51:13 -0400

    Ex9 - redone, commented out instructions

--- Exercise9.rb
+++ Exercise9.rb
@@ -1,39 +1,38 @@
 original_grocery_list = ["carrots", "juice", "salmon", "apple", "celery"]
 
-
+#0. present list with *
 grocery_list = original_grocery_list.map do |list|
     "* #{list}"
 end
-puts grocery_list
-puts "------"
+puts "0. " + grocery_list.to_s
 
+#1. adding items
 def additem(grocery_list, object)
   grocery_list << "* "+object
-  puts "* #{object}"
+  puts "1. " + "* #{object}"
 end
 
-
+#2. counting list
 additem(grocery_list,"honey")
 additem(grocery_list,"cranberry")
 
-puts "------"
+puts "2. count items: " + grocery_list.length.to_s
 
-
-puts grocery_list.length
-puts "------"
-
+#3. check for bananas
 case grocery_list.include?("banana")
   when false
-   puts "No bananas this week."
+   puts "3. Check for bananas: No bananas this week."
   when true
-   puts "Pick up bananas!"
+   puts "3. Check for bananas: Pick up bananas!"
  else
 end
-puts "------"
 
-puts grocery_list[1]
-puts "------"
-puts grocery_list.sort
-puts "------"
+#4. display second item on list
+puts "4. Display second item: #{grocery_list[1]}"
+
+#5. alphebetical order
+puts "5. Alphabetical order: #{grocery_list.sort}"
+
+#6. remove item: salmon
 grocery_list.delete("* salmon")
-puts grocery_list
+puts "6. Remove Salmon: #{grocery_list.sort}"
