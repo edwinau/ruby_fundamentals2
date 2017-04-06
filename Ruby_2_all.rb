9ab9fdb811ace1a079354db9b8b68159abe45419
EDWIN AU <edwinau@rogers.com>
2017-04-06 00:28:55 -0400

    redone 9

--- Ruby_2_all.rb
+++ Ruby_2_all.rb
@@ -30,3 +30,34 @@ end
 
 array_num = [250, 7.95, 30.95, 16.50]
 total_expense(array_num)
+
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+def asterisk(x)
+   x.each do |item|
+     puts "* #{item}"
+   end
+   return x
+end
+x = ["carrots", "toilet paper", "apples", "salmon"]
+asterisk(x)
+puts "the total item is #{x.count}."
+
+case x.include?("banana")
+when true
+  puts "yes, buy"
+when false
+  puts "no, next week"
+end
+
+x = grocery_list << "rice"
+puts asterisk(x)
+
+puts x[-2]
+puts x[-3]
+
+y = x.sort
+puts asterisk(y)
+
+y.delete("salmon")
+puts asterisk(y)
