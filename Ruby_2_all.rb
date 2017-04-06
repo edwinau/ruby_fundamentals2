7fdd54d28943563415433d343a4606b2af4680e5
EDWIN AU <edwinau@rogers.com>
2017-04-05 23:13:33 -0400

    redone ex6

--- Ruby_2_all.rb
+++ Ruby_2_all.rb
@@ -1,8 +1,14 @@
 def greet_backwards(name)
-  return name.reverse
+  return name.reverse*2
 end
 
 puts greet_backwards("Bob")
 puts greet_backwards("Shirley")
 puts greet_backwards("Sue")
 puts greet_backwards("Andy")
+
+def f_to_c(fah)
+  cel= (fah-32) *5/9
+  return puts "convert temp is #{cel}C."
+end
+puts f_to_c(15)
